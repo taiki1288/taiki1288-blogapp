fcbfc5b6feec06213342775f32cd5542f2bca995
taiki yamashita <taiki0808ty@gmail.com>
2021-01-09 22:53:40 +0900

    new action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'articles#index'
 
   resources :articles do
-    resources :comments, only[:new, :create]
+    resources :comments, only: [:new, :create]
   end
 end
