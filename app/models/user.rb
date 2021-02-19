aee31584c30cf2d7849d6837e5e36c1da0861efd
taiki yamashita <taiki0808ty@gmail.com>
2021-02-19 11:11:00 +0900

    フォローの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,10 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
+
+  has_many :following_rerationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_rerationships, source: :following
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -40,6 +44,11 @@ class User < ApplicationRecord
   def display_name
     profile&.nickname || self.email.split('@').first
   end
+
+  def follow!(user)
+    following_rerationships.create!(following_id: user.id)
+  end
+
 
   def prepare_profile
     profile || build_profile
