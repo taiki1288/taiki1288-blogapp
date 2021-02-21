bfed3ee2640542685e5be658e76aa0c0355d971e
taiki yamashita <taiki0808ty@gmail.com>
2021-02-22 06:06:13 +0900

    ajaxでいいねの状態

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,9 @@ require("@rails/actiontext")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 const handleHeartDisplay = (hasLiked) => {
     if (hasLiked) {
@@ -33,10 +36,32 @@ const handleHeartDisplay = (hasLiked) => {
 document.addEventListener('turbolinks:load', () => {
     const dataset = $('#article-show').data()
     const articleId = dataset.articleId
-    
+
     axios.get(`/articles/${articleId}/like`)
         .then((response) => {
           const hasLiked = response.data.hasLiked
           handleHeartDisplay(hasLiked)
         })
+
+    $('.inactive-heart').on('click', () => {
+      axios.post(`/articles/${articleId}/like`)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((e) => {
+            window.alert('Error')
+            console.log(e)
+        })
+    })
+
+    $('.active-heart').on('click', () => {
+      axios.delete(`/articles/${articleId}/like`)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((e) => {
+            window.alert('Error')
+            console.log(e)
+        })
+    })
 })
